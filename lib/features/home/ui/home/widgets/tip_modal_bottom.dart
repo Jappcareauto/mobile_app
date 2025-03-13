8cbf51cdaaacb32ccbbbc9adc66201add304f498
Fenyep <0Uiimi1D5/-yYkGt>
2025-03-13 12:48:37 +0100

    Fixed the tip modal bottom overflow error

--- lib/features/home/ui/home/widgets/tip_modal_bottom.dart
+++ lib/features/home/ui/home/widgets/tip_modal_bottom.dart
@@ -9,42 +9,39 @@ class TipModalBottomWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        width: Get.width,
-        height: 390,
-        padding: const EdgeInsets.symmetric(horizontal: 20 , vertical: 0),
-        child: Column(
-          children: [
-            Container(
-              width: 28,
-              height: 5,
-
-              decoration:BoxDecoration(
-                color: AppColors.black,
-
-                borderRadius: BorderRadius.circular(6)
-              ) ,
-            ),
-            const SizedBox(height: 20,),
-            Text("Tip",
-                style: Get.textTheme.titleLarge
-                    ?.copyWith(fontWeight: FontWeight.bold)),
-            const SizedBox(height: 20),
-            Text(
-                "Rotate your tires regularly to ensure they wear evenly and last longer.",
-                style: Get.textTheme.bodyMedium?.copyWith(
-                    color: Get.theme.primaryColor,
-                    fontWeight: FontWeight.bold)),
-            const SizedBox(height: 20),
-            Text(
-                "Regularly rotating your tires is essential for maintaining even wear and extending their lifespan. By rotating your tires consistently, you can help distribute the wear more evenly across all four tires, resulting in a longer lifespan for each tire. This practice not only promotes safety but also helps you get the most out of your investment in tires.",
-                style: Get.textTheme.bodyMedium),
-            const SizedBox(height: 20),
-            CustomButton(
-                text: "Done",
-                onPressed: Get.back,
-
-            ),
-          ],
-        ));
+      width: Get.width,
+      // height: 390,
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+      child: Column(
+        children: [
+          Container(
+            width: 28,
+            height: 5,
+            decoration: BoxDecoration(
+                color: AppColors.black, borderRadius: BorderRadius.circular(6)),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Text("Tip",
+              style: Get.textTheme.titleLarge
+                  ?.copyWith(fontWeight: FontWeight.bold)),
+          const SizedBox(height: 20),
+          Text(
+              "Rotate your tires regularly to ensure they wear evenly and last longer.",
+              style: Get.textTheme.bodyMedium?.copyWith(
+                  color: Get.theme.primaryColor, fontWeight: FontWeight.bold)),
+          const SizedBox(height: 20),
+          Text(
+              "Regularly rotating your tires is essential for maintaining even wear and extending their lifespan. By rotating your tires consistently, you can help distribute the wear more evenly across all four tires, resulting in a longer lifespan for each tire. This practice not only promotes safety but also helps you get the most out of your investment in tires.",
+              style: Get.textTheme.bodyMedium),
+          const SizedBox(height: 20),
+          CustomButton(
+            text: "Done",
+            onPressed: Get.back,
+          ),
+        ],
+      ),
+    );
   }
 }
