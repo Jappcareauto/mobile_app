89e917729fa2e0c36a54583e2f14cb3b22fe1526
Fenyep <0Uiimi1D5/-yYkGt>
2025-03-23 07:01:32 +0100

    Update of the api url

--- lib/core/utils/app_constants.dart
+++ lib/core/utils/app_constants.dart
@@ -2,7 +2,7 @@
 
 class AppConstants {
   static const String appVersion = "1.0.0";
-  static const String baseUrl = "https://api.jappcare.com/api/v1";
+  static const String baseUrl = "https://bpi.jappcare.com/api/v1";
   //"https://japcare-be.onrender.com/api/v1";
   // "http://ec2-3-23-222-64.us-east-2.compute.amazonaws.com:8555/api/v1";
   static const String notFoundScreen = "/404";
--- lib/features/chat/domain/core/utils/chat_constants.dart
+++ lib/features/chat/domain/core/utils/chat_constants.dart
@@ -1,9 +1,9 @@
 //Don't translate me
 class ChatConstants {
-
   static const String featureName = 'chat';
   static const String featureVersion = '1.0.0';
-  static const String chatUri = 'wss://api.jappcare.com/api/v1/chat?chatRoomId=';
+  static const String chatUri =
+      'wss://bpi.jappcare.com/api/v1/chat?chatRoomId=';
 
   static const String sendMessagePostUri = '/chat-message';
   // Add other constants here
