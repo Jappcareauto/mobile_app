7c9974da0cf9f5526e2dad695769391eb8d67a4a
ndifoinhilary <ndifoinhilary@gmail.com>
2026-02-02 01:08:08 +0100

    navigation error fix

--- lib/features/home/navigation/bindings/home_controller_binding.dart
+++ lib/features/home/navigation/bindings/home_controller_binding.dart
@@ -1,9 +1,14 @@
 import 'package:get/get.dart';
 import '../../ui/home/controllers/home_controller.dart';
+import '../../ui/dashboard/controllers/dashboard_controller.dart';
 
 class HomeControllerBinding extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut<HomeController>(() => HomeController(Get.find()));
+    // DashboardController is needed by HomeScreen for navigation
+    if (!Get.isRegistered<DashboardController>()) {
+      Get.lazyPut<DashboardController>(() => DashboardController(Get.find()));
+    }
   }
 }
